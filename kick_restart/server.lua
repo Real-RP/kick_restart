a7aba681a01d183d3089936fcab21303232023e9
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-03-08 23:27:31 +0100

    Update server.lua

--- kick_restart/server.lua
+++ kick_restart/server.lua
@@ -10,27 +10,33 @@
 
 --]]
 
-lang = 'en' -- languages are: en = english, de = deutsch, fr = französisch
+lang = 'de' -- languages are: en = english, de = deutsch, fr = französisch
 
 debug.cfg = true --gives acces to use /checkperms, that will show if you are allowed to use the restart ressource or not.
-debug.msg = false --gives a error message for players that are not allowed for a restart.
-
+debug.msg = true --gives a error message for players that are not allowed for a restart.
+-------------------------------------------------------------------------------------------------------------------------------------
 --Locales: English
 if lang == 'en' then
 kick_message = "All Roleplay situations ended automatically. Your progress has been saved. Reason: Server restart."
 kick_message_before_join = "The Server is restarting! please retry later."
+permissions = "^3SYSTEM: ^1You have no permissions!"
+have_permissions = "^3SYSTEM: ^2You have permissions!"
 end
 
 --Locales: Deutsch
 if lang == 'de' then
 kick_message = "Alle RP Situationen wurden automatisch beendet, dein Fortschritt wurde gespeichert, Grund: Server Neustart"
 kick_message_before_join = "Der Server startet gerade Neu, bitte connecte später erneut."
+permissions = "^3SYSTEM: ^1Du hast keine Berechtigungen!"
+have_permissions = "^3SYSTEM: ^2Du hast Berechtigungen!"
 end
 
 --Locales: français
 if lang == 'fr' then
 kick_message = "Toutes les situations RP ont pris fin automatiquement, votre progression a été enregistrée, motif: redémarrage du serveur"
 kick_message_before_join = "Le serveur est en train de démarrer, veuillez vous reconnecter plus tard."
+permissions = "^3SYSTEM: ^1Vous n'avez aucune permission!"
+have_permissions = "^3SYSTEM: ^2Vous avez des permissions!"
 end
 
 
@@ -43,6 +49,8 @@ local kick = false
 RegisterCommand("beginrestart", function(source, args, rawCommand)
     if IsPlayerAceAllowed(source, "restart.cmds") then
 		kick = true
+	else
+		TriggerClientEvent("chatMessage", source, permissions)
 	end
 end)
 
@@ -58,7 +66,7 @@ end)
 RegisterCommand("closeconnect", function(source, args, rawCommand)
 	if IsPlayerAceAllowed(source, "restart.cmds") then
 		AddEventHandler("playerConnecting", function(playerName, setKickReason, deferrals)
-			deferrals.done(kick_message_before_join) --shows why you can't connect again. --translate
+			deferrals.done(kick_message_before_join)
 		end)
 	end
 end)
@@ -69,9 +77,9 @@ end)
 if debug.cfg then
 RegisterCommand("checkperms", function(source, args, rawCommand)
     if IsPlayerAceAllowed(source, "restart.cmds") then
-        	TriggerClientEvent("chatMessage", source, "^2You have permissions!") --translate --do not translate "chatMessage" !!!!
-    else
-        TriggerClientEvent("chatMessage", source, "^1You have no permssions.") --translate --do not translate "chatMessage" !!!!
+        	TriggerClientEvent("chatMessage", source, have_permissions)
+		else
+			TriggerClientEvent("chatMessage", source, permissions)
 		end
 	end)
 end

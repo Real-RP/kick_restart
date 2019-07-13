a624ff7ef16aeb68ff0af42c78f8999c15620633
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-07-13 23:02:18 +0200

    Update server.lua

--- kick_restart/server.lua
+++ kick_restart/server.lua
@@ -12,14 +12,14 @@
 
 lang = 'de' -- languages are: en = english, de = deutsch, fr = français
 local timebeforenextconnect = 15 --time in seconds before the next can connect after a planned restart
-userestartplanner = false --change to true to activate the table planner below:
+userestartplanner = true --change to true to activate the table planner below:
 
-restarttimes = {'21:58:00', '21:58:30'} --add your restart times here. [FORMAT: HOUR:MINUTE:SECOND]
+restarttimes = {'22:59:00', '22:59:30'} --add your restart times here. [FORMAT: HOUR:MINUTE:SECOND]
 
 
 debug.cfg = true --gives acces to use /checkperms, that will show if you are allowed to use the restart ressource or not.
 debug.msg = true --gives a error message for players that are not allowed for a restart.
-debug.time = false --change to true to print the time in the console as a debug test.
+debug.time = true --change to true to print the time in the console as a debug test.
 -------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -91,6 +91,15 @@ Citizen.CreateThread(function()
 	end
 end)
 
+RegisterCommand("test", function(source)
+	for i = 0, 255 do
+		local ped = GetPlayerPed(-1)
+	end
+	if ped ~= nil then
+		print(ped)
+	end
+end)
+
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1000)
@@ -113,8 +122,9 @@ end)
 AddEventHandler("playerConnecting", function(playerName, setKickReason, deferrals)
 	if stopconnect == true then
 			deferrals.done(kick_message_before_join)
-			Wait(timebeforenextconnect * 1000)
+			Wait(15000)
 			stopconnect = false
+			kick = false
 	else
 			deferrals.done()
 	end
